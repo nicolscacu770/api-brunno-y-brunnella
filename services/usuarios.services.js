bb375c4dc20e196072854737f6c4c1270a6ba84b
Nicolas <nicolas.cacua@uptc.edu.co>
2023-03-11 08:33:54 -0500

    validaciones

--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -4,14 +4,26 @@ const {pool} = require('./connectBD');
 const create = async (req, res) => {
     try{
         const body = req.body;
-        const query = `INSERT INTO usuarios VALUES ('${body.id}', '${body.nombre}', '${body.apellido}', '${body.fecha_nacimiento}', '${body.sexo}', '${body.correo}', '${body.password}', '${body.tipoUsuario}')`;
-        //const queryUsuarios = `INSERT INTO usuarios VALUES ('${body.id}', '${body.correo}', '${body.password}', 'estudiante')`;  //crea el usuario en otra tabla de usuarios generales que permite el login de todo tipo  de usuario
-        const [rows] = await pool.query(query);
-        res.status(201).send(rows);
+
+        if(body.id == undefined || body.nombre == undefined || body.apellido == undefined || body.correo == undefined || body.password == undefined ){
+            //console.log(body.id, ", "body. )
+            return res.status(500).json({message: `datos faltantes`})
+        }else{
+            const query = `INSERT INTO usuarios VALUES ('${body.id}', '${body.nombre}', '${body.apellido}', '${body.fecha_nacimiento}', '${body.sexo}', '${body.correo}', '${body.password}', '${body.tipoUsuario}')`;
+            //const queryUsuarios = `INSERT INTO usuarios VALUES ('${body.id}', '${body.correo}', '${body.password}', 'estudiante')`;  //crea el usuario en otra tabla de usuarios generales que permite el login de todo tipo  de usuario
+            const [rows] = await pool.query(query);
+            res.status(201).send(rows);
+        }
     }catch (error) {
         console.log(error);
         if(error.errno === 1062){
             return res.status(500).json({message: `el usuario con el id: ${req.body.id} ya existe`});
+        }else if(error.errno === 1366 ){
+            return res.status(422).json({message: `el tipo de datos no coincide (String -> int)`});
+        }else if(error.errno === 1292 ){
+            return res.status(422).json({message: `el formato del campo fecha es invalido. es: (año-mes-dia)`});
+        }else if(error.errno === 1406 ){
+            return res.status(422).json({message: `el dato excede la longitud para la col sexo (char)`});
         }else{
             return res.status(500).json({message: 'Algo ha salido mal. ruta: usuariosServices/create'});
         }
@@ -23,7 +35,12 @@ const find = async (req, res) => {
     try{
         const query = 'SELECT * from usuarios';
         const [rows] = await pool.query(query);
-        res.send({ rows });
+
+        if(rows.length <= 0 ){
+            res.status(404).send('usuarios no registrados');
+        }else{
+            res.json(rows);
+        }
     }catch (error) {
         console.error(error);
         return res.status(500).json({message: 'Algo ha salido mal. ruta: usuariosServices/find' + error});
@@ -34,7 +51,7 @@ const find = async (req, res) => {
 const findOne = async (req, res) => {
     try{
         idUsuario = req.params.id;
-        const query = `SELECT * FROM usuarios where id = '${iidUsuario}'`;
+        const query = `SELECT * FROM usuarios where id = '${idUsuario}'`;
         const [rows] = await pool.query(query);
     
         if(rows.length <= 0 ){
