99464067739d2286561883732fcdffe526966743
Nicolas <nicolas.cacua@uptc.edu.co>
2023-04-21 09:00:15 -0500

    fix an error

--- services/prendas.services.js
+++ services/prendas.services.js
@@ -36,8 +36,9 @@ const create = async (req, res) => {
 
             let imageUrl = "";
             if(req.file){
+                console.log(req.file);
            //     imageUrl = 'http://localhost:3001/api/imagenes/' + req.file.filename;
-                imageUrl = 'api-brunno-y-brunnella-production.up.railway.app' + req.file.filename;
+                imageUrl = 'api-brunno-y-brunnella-production.up.railway.app/api/imagenes/' + req.file.filename;
                 
             }
 
