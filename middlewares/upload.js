d0194feb815249ec36a80bc2364623b015d708a6
Nicolas <nicolas.cacua@uptc.edu.co>
2023-04-21 09:05:32 -0500

    same

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,10 +1,13 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
+ 
     destination: function(req, file, cb){
+        console.log('abrió multer');
         cb(null, 'imagenes/');
     },
     filename: function (req, file, cb) {
+        console.log(file.originalname);
         const extension = file.originalname.slice(file.originalname.lastIndexOf(".")); ///toma la extension del archivo, que aparece en el campo originalname
         cb( null, file.originalname + '-'+ Date.now() + extension );
     }
